980467ced0b211cf5cc4bd30c52d12917c9ff13c
Blake Mallory <blakecmallory@gmail.com>
2022-11-30 14:08:15 -0500

    added option to save the audio file first

--- mic.py
+++ mic.py
@@ -15,7 +15,11 @@ import numpy as np
 @click.option("--energy", default=300, help="Energy level for mic to detect", type=int)
 @click.option("--dynamic_energy", default=False,is_flag=True, help="Flag to enable dynamic engergy", type=bool)
 @click.option("--pause", default=0.8, help="Pause time before entry ends", type=float)
-def main(model, english,verbose, energy, pause,dynamic_energy):
+@click.option("--save_file",default=False, help="Flag to save file", is_flag=True,type=bool)
+def main(model, english,verbose, energy, pause,dynamic_energy,save_file):
+    if save_file:
+        temp_dir = tempfile.mkdtemp()
+        save_path = os.path.join(temp_dir, "temp.wav")
     #there are no english models for large
     if model != "large" and english:
         model = model + ".en"
@@ -32,12 +36,19 @@ def main(model, english,verbose, energy, pause,dynamic_energy):
         while True:
             #get and save audio to wav file
             audio = r.listen(source)
-            torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
+            if save_file:
+                data = io.BytesIO(audio.get_wav_data())
+                audio_clip = AudioSegment.from_file(data)
+                audio_clip.export(save_path, format="wav") 
+                audio_data = save_path               
+            else:
+                torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
+                audio_data = torch_audio
 
             if english:
-                result = audio_model.transcribe(torch_audio,language='english')
+                result = audio_model.transcribe(audio_data,language='english')
             else:
-                result = audio_model.transcribe(torch_audio)
+                result = audio_model.transcribe(audio_data)
 
             if not verbose:
                 predicted_text = result["text"]
