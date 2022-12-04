269e8d26963a87809a64936faf81367cac916c27
Mathias Arens <mathias@arens.ma>
2022-12-04 14:19:59 +0100

    Use threading to record audio in parallel to transcribing/translating it. Previously, during the blocking synchronous call to the model, the microphone input was missed.

--- mic.py
+++ mic.py
@@ -2,8 +2,10 @@ import io
 from pydub import AudioSegment
 import speech_recognition as sr
 import whisper
+import queue
 import tempfile
 import os
+import threading
 import click
 import torch
 import numpy as np
@@ -17,14 +19,23 @@ import numpy as np
 @click.option("--pause", default=0.8, help="Pause time before entry ends", type=float)
 @click.option("--save_file",default=False, help="Flag to save file", is_flag=True,type=bool)
 def main(model, english,verbose, energy, pause,dynamic_energy,save_file):
-    if save_file:
-        temp_dir = tempfile.mkdtemp()
-        save_path = os.path.join(temp_dir, "temp.wav")
+    temp_dir = tempfile.mkdtemp() if save_file else None
     #there are no english models for large
     if model != "large" and english:
         model = model + ".en"
-    audio_model = whisper.load_model(model)    
-    
+    audio_model = whisper.load_model(model)
+    audio_queue = queue.Queue()
+    result_queue = queue.Queue()
+    threading.Thread(target=record_audio,
+                     args=(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir)).start()
+    threading.Thread(target=transcribe_forever,
+                     args=(audio_queue, result_queue, audio_model, english, verbose, save_file)).start()
+
+    while True:
+        print(result_queue.get())
+
+
+def record_audio(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir):
     #load the speech recognizer and set the initial energy threshold and pause threshold
     r = sr.Recognizer()
     r.energy_threshold = energy
@@ -33,27 +44,40 @@ def main(model, english,verbose, energy, pause,dynamic_energy,save_file):
 
     with sr.Microphone(sample_rate=16000) as source:
         print("Say something!")
+        i = 0
         while True:
             #get and save audio to wav file
             audio = r.listen(source)
             if save_file:
                 data = io.BytesIO(audio.get_wav_data())
                 audio_clip = AudioSegment.from_file(data)
-                audio_clip.export(save_path, format="wav") 
-                audio_data = save_path               
+                filename = os.path.join(temp_dir, f"temp{i}.wav")
+                audio_clip.export(filename, format="wav")
+                audio_data = filename
             else:
                 torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
                 audio_data = torch_audio
 
-            if english:
-                result = audio_model.transcribe(audio_data,language='english')
-            else:
-                result = audio_model.transcribe(audio_data)
+            audio_queue.put_nowait(audio_data)
+            i += 1
 
-            if not verbose:
-                predicted_text = result["text"]
-                print("You said: " + predicted_text)
-            else:
-                print(result)
-                
+
+def transcribe_forever(audio_queue, result_queue, audio_model, english, verbose, save_file):
+    while True:
+        audio_data = audio_queue.get()
+        if english:
+            result = audio_model.transcribe(audio_data,language='english')
+        else:
+            result = audio_model.transcribe(audio_data)
+
+        if not verbose:
+            predicted_text = result["text"]
+            result_queue.put_nowait("You said: " + predicted_text)
+        else:
+            result_queue.put_nowait(result)
+
+        if save_file:
+            os.remove(audio_data)
+
+
 main()
