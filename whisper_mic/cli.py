c7642ae22e7927dd08ee5bf54a2d05bba89ecb3a
Blake Mallory <blakecmallory@gmail.com>
2023-06-29 14:22:17 -0400

    put logic into a class

--- whisper_mic/cli.py
+++ whisper_mic/cli.py
@@ -10,6 +10,8 @@ import click
 import torch
 import numpy as np
 
+from whisper_mic.whisper_mic import WhisperMic
+
 @click.command()
 @click.option("--model", default="base", help="Model to use", type=click.Choice(["tiny","base", "small","medium","large"]))
 @click.option("--device", default=("cuda" if torch.cuda.is_available() else "cpu"), help="Device to use", type=click.Choice(["cpu","cuda"]))
@@ -20,65 +22,8 @@ import numpy as np
 @click.option("--pause", default=0.8, help="Pause time before entry ends", type=float)
 @click.option("--save_file",default=False, help="Flag to save file", is_flag=True,type=bool)
 def main(model, english,verbose, energy, pause,dynamic_energy,save_file,device):
-    temp_dir = tempfile.mkdtemp() if save_file else None
-    #there are no english models for large
-    if model != "large" and english:
-        model = model + ".en"
-    audio_model = whisper.load_model(model).to(device)
-    audio_queue = queue.Queue()
-    result_queue = queue.Queue()
-    threading.Thread(target=record_audio,
-                     args=(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir)).start()
-    threading.Thread(target=transcribe_forever,
-                     args=(audio_queue, result_queue, audio_model, english, verbose, save_file)).start()
-
-    while True:
-        print(result_queue.get())
-
-
-def record_audio(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir):
-    #load the speech recognizer and set the initial energy threshold and pause threshold
-    r = sr.Recognizer()
-    r.energy_threshold = energy
-    r.pause_threshold = pause
-    r.dynamic_energy_threshold = dynamic_energy
-
-    with sr.Microphone(sample_rate=16000) as source:
-        print("Say something!")
-        i = 0
-        while True:
-            #get and save audio to wav file
-            audio = r.listen(source)
-            if save_file:
-                data = io.BytesIO(audio.get_wav_data())
-                audio_clip = AudioSegment.from_file(data)
-                filename = os.path.join(temp_dir, f"temp{i}.wav")
-                audio_clip.export(filename, format="wav")
-                audio_data = filename
-            else:
-                torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
-                audio_data = torch_audio
-
-            audio_queue.put_nowait(audio_data)
-            i += 1
-
-
-def transcribe_forever(audio_queue, result_queue, audio_model, english, verbose, save_file):
-    while True:
-        audio_data = audio_queue.get()
-        if english:
-            result = audio_model.transcribe(audio_data,language='english')
-        else:
-            result = audio_model.transcribe(audio_data)
-
-        if not verbose:
-            predicted_text = result["text"]
-            result_queue.put_nowait("You said: " + predicted_text)
-        else:
-            result_queue.put_nowait(result)
-
-        if save_file:
-            os.remove(audio_data)
+    mic = WhisperMic(model=model, english=english, verbose=verbose, energy=energy, pause=pause, dynamic_energy=dynamic_energy, save_file=save_file, device=device)
+    mic.listen_loop()
 
 if __name__ == "__main__":
     main()
---
+++ whisper_mic/whisper_mic.py
@@ -0,0 +1,128 @@
+import torch
+import whisper
+import queue
+import speech_recognition as sr
+import threading
+import io
+import numpy as np
+from pydub import AudioSegment
+import os
+import tempfile
+import time
+
+
+class WhisperMic:
+    def __init__(self,model="base",device=("cuda" if torch.cuda.is_available() else "cpu"),english=False,verbose=False,energy=300,pause=0.8,dynamic_energy=False,save_file=False):
+        self.energy = energy
+        self.pause = pause
+        self.dynamic_energy = dynamic_energy
+        self.save_file = save_file
+        self.verbose = verbose
+        self.english = english
+
+        if model != "large" and self.english:
+            model = model + ".en"
+
+        self.audio_model = whisper.load_model(model).to(device)
+        self.temp_dir = tempfile.mkdtemp() if save_file else None
+
+        self.audio_queue = queue.Queue()
+        self.result_queue = queue.Queue()
+
+        self.break_threads = False
+        self.mic_active = False
+
+        self.mic_thread = threading.Thread(target=self.record_audio, daemon=True)
+        self.mic_thread.start()
+
+
+    def record_audio(self):
+        r = sr.Recognizer()
+        r.energy_threshold = self.energy
+        r.pause_threshold = self.pause
+        r.dynamic_energy_threshold = self.dynamic_energy
+        
+        with sr.Microphone(sample_rate=16000) as source:
+            self.mic_active = True
+            print("Say something!")
+            i = 0
+            while True:
+                if not self.mic_active:
+                    break
+                #get and save audio to wav file
+                audio = r.listen(source)
+                if self.save_file:
+                    data = io.BytesIO(audio.get_wav_data())
+                    audio_clip = AudioSegment.from_file(data)
+                    filename = os.path.join(self.temp_dir, f"temp{i}.wav")
+                    audio_clip.export(filename, format="wav")
+                    audio_data = filename
+                else:
+                    torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
+                    audio_data = torch_audio
+                
+                self.audio_queue.put_nowait(audio_data)
+                i += 1
+
+
+
+    def transcribe_forever(self):
+        while True:
+            if self.break_threads:
+                break
+            audio_data = self.audio_queue.get()
+            if self.english:
+                result = self.audio_model.transcribe(audio_data,language='english')
+            else:
+                result = self.audio_model.transcribe(audio_data)
+
+            if not self.verbose:
+                predicted_text = result["text"]
+                self.result_queue.put_nowait("You said: " + predicted_text)
+            else:
+                self.result_queue.put_nowait(result)
+
+            if self.save_file:
+                os.remove(audio_data)
+
+    def transcribe(self):
+        audio_data = self.audio_queue.get()
+        if self.english:
+            result = self.audio_model.transcribe(audio_data,language='english')
+        else:
+            result = self.audio_model.transcribe(audio_data)
+
+        if not self.verbose:
+            predicted_text = result["text"]
+            self.result_queue.put_nowait("You said: " + predicted_text)
+        else:
+            self.result_queue.put_nowait(result)
+
+        if self.save_file:
+            os.remove(audio_data)
+
+
+    def listen_loop(self):
+        threading.Thread(target=self.transcribe_forever).start()
+
+        while True:
+            print(self.result_queue.get())
+
+    def listen(self):
+        transcribe_thread = threading.Thread(target=self.transcribe)
+        transcribe_thread.start()
+
+        while True:
+            if not self.result_queue.empty():
+                transcribe_thread.join()
+                return self.result_queue.get()
+            
+    def toggle_microphone(self):
+        self.mic_active = not self.mic_active
+        if self.mic_active:
+            print("Mic on")
+        else:
+            print("turning off mic")
+            self.mic_thread.join()
+            print("Mic off")
+    
