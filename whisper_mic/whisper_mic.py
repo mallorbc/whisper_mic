86b8b3d372a72d9914a1db83eb4a455d14171de6
Sankhadeep Dutta <dutta.shankhadeep@gmail.com>
2023-08-20 22:32:10 +0530

    commit 1: fixed the listen method.

--- whisper_mic/whisper_mic.py
+++ whisper_mic/whisper_mic.py
@@ -3,12 +3,9 @@ import whisper
 import queue
 import speech_recognition as sr
 import threading
-import io
 import numpy as np
-from pydub import AudioSegment
 import os
 import tempfile
-import time
 import platform
 import pynput.keyboard
 
@@ -16,7 +13,7 @@ from whisper_mic.utils import get_logger
 
 
 class WhisperMic:
-    def __init__(self,model="base",device=("cuda" if torch.cuda.is_available() else "cpu"),english=False,verbose=False,energy=300,pause=0.8,dynamic_energy=False,save_file=False, model_root="~/.cache/whisper",mic_index=None):
+    def __init__(self,model="base",device=("cuda" if torch.cuda.is_available() else "cpu"),english=False,verbose=False,energy=300,pause=2,dynamic_energy=False,save_file=False, model_root="~/.cache/whisper",mic_index=None):
         self.logger = get_logger("whisper_mic", "info")
         self.energy = energy
         self.pause = pause
@@ -42,16 +39,16 @@ class WhisperMic:
 
         self.audio_queue = queue.Queue()
         self.result_queue: "queue.Queue[str]" = queue.Queue()
-
+        
         self.break_threads = False
         self.mic_active = False
 
         self.banned_results = [""," ","\n",None]
 
-        self.setup_mic(mic_index)
+        self.__setup_mic(mic_index)
 
 
-    def setup_mic(self, mic_index):
+    def __setup_mic(self, mic_index):
         if mic_index is None:
             self.logger.info("No mic index provided, using default")
         self.source = sr.Microphone(sample_rate=16000, device_index=mic_index)
@@ -64,18 +61,17 @@ class WhisperMic:
         with self.source:
             self.recorder.adjust_for_ambient_noise(self.source)
 
-        self.recorder.listen_in_background(self.source, self.record_callback, phrase_time_limit=2)
-        self.logger.info("Mic setup complete, you can now talk")
+        self.logger.info("Mic setup complete")
 
 
-    def preprocess(self, data):
+    def __preprocess(self, data):
         return torch.from_numpy(np.frombuffer(data, np.int16).flatten().astype(np.float32) / 32768.0)
 
-    def get_all_audio(self, min_time: float = -1.):
+    
+    def __get_all_audio(self):
         audio = bytes()
         got_audio = False
-        time_start = time.time()
-        while not got_audio or time.time() - time_start < min_time:
+        while not got_audio:
             while not self.audio_queue.empty():
                 audio += self.audio_queue.get()
                 got_audio = True
@@ -83,26 +79,47 @@ class WhisperMic:
         data = sr.AudioData(audio,16000,2)
         data = data.get_raw_data()
         return data
+    
+
+    # Handles the task of getting the audio input via microphone. This method has been used both for listen() method and for the __transcribe_forever() method (which has been used for implementing the listen_loop() method)
+    def __listen_handler(self, timeout, phrase_time_limit):
+        with self.source as microphone:
+            audio = self.recorder.listen(source=microphone, timeout=timeout, phrase_time_limit=phrase_time_limit)
+        
+        self.__record_load(audio)
+        audio_data = self.__get_all_audio()
+        self.__transcribe(data=audio_data)
 
 
-    def record_callback(self,_, audio: sr.AudioData) -> None:
+    # This method is similar to the __listen_handler() method but it has the added ability for recording the audio for a specified duration of time
+    def __record_handler(self, duration, offset):
+        with self.source as microphone:
+            audio = self.recorder.record(source=microphone, duration=duration, offset=offset)
+        
+        self.__record_load(audio)
+        audio_data = self.__get_all_audio()
+        self.__transcribe(data=audio_data)
+
+
+    # This method takes the recorded audio data, converts it into raw format and stores it in a queue. 
+    def __record_load(self, audio: sr.AudioData) -> None:
         data = audio.get_raw_data()
         self.audio_queue.put_nowait(data)
 
 
-    def transcribe_forever(self) -> None:
+    def __transcribe_forever(self,timeout, phrase_time_limit) -> None:
         while True:
             if self.break_threads:
                 break
-            self.transcribe()
+            self.__listen_handler(timeout, phrase_time_limit)
 
 
-    def transcribe(self,data=None, realtime: bool = False) -> None:
+    def __transcribe(self,data=None, realtime: bool = False) -> None:
         if data is None:
-            audio_data = self.get_all_audio()
+            audio_data = self.__get_all_audio()
         else:
             audio_data = data
-        audio_data = self.preprocess(audio_data)
+        audio_data = self.__preprocess(audio_data)
         if self.english:
             result = self.audio_model.transcribe(audio_data,language='english')
         else:
@@ -120,8 +137,9 @@ class WhisperMic:
             os.remove(audio_data)
 
 
-    def listen_loop(self, dictate: bool = False) -> None:
-        threading.Thread(target=self.transcribe_forever).start()
+    def listen_loop(self, dictate: bool = False, timeout=None, phrase_time_limit=None) -> None:
+        self.logger.info("Listening...")
+        threading.Thread(target=self.__transcribe_forever, args=(timeout, phrase_time_limit)).start()
         while True:
             result = self.result_queue.get()
             if dictate:
@@ -129,13 +147,23 @@ class WhisperMic:
             else:
                 print(result)
 
-
-    def listen(self, timeout: int = 3):
-        audio_data = self.get_all_audio(timeout)
-        self.transcribe(data=audio_data)
+            
+    def listen(self, timeout = None, phrase_time_limit=None):
+        self.logger.info("Listening...")
+        self.__listen_handler(timeout, phrase_time_limit)
         while True:
             if not self.result_queue.empty():
                 return self.result_queue.get()
+
+
+    # This method is similar to the listen() method, but it has the ability to listen for a specified duration, mentioned in the "duration" parameter.
+    def record(self, duration=None, offset=None):
+        self.logger.info("Listening...")
+        self.__record_handler(duration, offset)
+        while True:
+            if not self.result_queue.empty():
+                return self.result_queue.get()
+
 
     def toggle_microphone(self) -> None:
         #TO DO: make this work
