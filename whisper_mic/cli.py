ffcf5b89286c1ee2cade00f5bea35fd4bfded277
Jhong Lin <qcp87623abc@gmail.com>
2024-04-11 20:24:28 +0800

    DEL: remove dependency of `rich` logger

--- whisper_mic/cli.py
+++ whisper_mic/cli.py
@@ -8,16 +8,16 @@ from typing import Optional
 from whisper_mic import WhisperMic
 
 @click.command()
-@click.option("--model", default="base", help="Model to use", type=click.Choice(["tiny","base", "small","medium","large","large-v2","large-v3"]))
-@click.option("--device", default=("cuda" if torch.cuda.is_available() else "cpu"), help="Device to use", type=click.Choice(["cpu","cuda","mps"]))
-@click.option("--language", default=None, help="Determine the prior language to detect", type=str)
-@click.option("--energy", default=300, help="Energy level for mic to detect", type=int)
-@click.option("--dynamic_energy", default=False,is_flag=True, help="Flag to enable dynamic energy")
-@click.option("--pause", default=0.8, help="Pause time before entry ends", type=float)
-@click.option("--mic_index", default=None, help="Mic index to use", type=int)
-@click.option("--list_devices",default=False, help="Flag to list devices", is_flag=True)
-@click.option("--faster",default=False, help="Use faster_whisper implementation", is_flag=True)
-@click.option("--hallucinate_threshold",default=400, help="Raise this to reduce hallucinations.  Lower this to activate more often.", type=int)
+@click.option('--model', default='base', help='Model to use', type=click.Choice(['tiny','base', 'small','medium','large','large-v2','large-v3']))
+@click.option('--device', default=('cuda' if torch.cuda.is_available() else 'cpu'), help='Device to use', type=click.Choice(['cpu','cuda','mps']))
+@click.option('--language', default=None, help='Determine the prior language to detect', type=str)
+@click.option('--energy', default=300, help='Energy level for mic to detect', type=int)
+@click.option('--dynamic_energy', default=False, is_flag=True, help='Flag to enable dynamic energy')
+@click.option('--pause', default=0.8, help='Pause time before entry ends', type=float)
+@click.option('--mic_index', default=None, help='Mic index to use', type=int)
+@click.option('--list_devices', default=False, help='Flag to list devices', is_flag=True)
+@click.option('--faster', default=False, help='Use faster_whisper implementation', is_flag=True)
+@click.option('--hallucinate_threshold',default=400, help='Raise this to reduce hallucinations.  Lower this to activate more often.', type=int)
 def main(
     model: str,
     language: str,
@@ -33,7 +33,7 @@ def main(
     if list_devices:
         py_audio_instance = pyaudio.PyAudio()
         for mic_index in range(py_audio_instance.get_device_count()):
-            print(f"{mic_index}: {py_audio_instance.get_device_info_by_index(mic_index).get('name')}")
+            print(f'{mic_index}: {py_audio_instance.get_device_info_by_index(mic_index).get("name")}')
 
         return
 
@@ -45,7 +45,7 @@ def main(
         dynamic_energy=dynamic_energy,
         device=device,
         mic_index=mic_index,
-        implementation=("faster_whisper" if faster else "whisper"),
+        implementation=('faster_whisper' if faster else 'whisper'),
         hallucinate_threshold=hallucinate_threshold,
     )
 
@@ -53,8 +53,8 @@ def main(
         mic.listen_loop(phrase_time_limit=2)
 
     except KeyboardInterrupt:
-        print("Interrupted by keyboard.")
+        print('Interrupted by keyboard.')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- whisper_mic/whisper_mic.py
+++ whisper_mic/whisper_mic.py
@@ -3,27 +3,23 @@ import queue
 import speech_recognition as sr
 import threading
 import numpy as np
-import os
 import time
 import platform
-
-from utils import get_logger
 
 
 class WhisperMic:
     def __init__(
         self,
-        model="base",
-        device=("cuda" if torch.cuda.is_available() else "cpu"),
+        model='base',
+        device=('cuda' if torch.cuda.is_available() else 'cpu'),
         language=None,
         energy=300,
         pause=2,
         dynamic_energy=False,
         mic_index=None,
-        implementation="whisper",
+        implementation='whisper',
         hallucinate_threshold=300
     ):
-        self.logger = get_logger("whisper_mic", "info")
         self.energy = energy
         self.hallucinate_threshold = hallucinate_threshold
         self.pause = pause
@@ -33,14 +29,14 @@ class WhisperMic:
         self.platform = platform.system()
 
         self.faster = False
-        if (implementation == "faster_whisper"):
+        if (implementation == 'faster_whisper'):
             try:
                 from faster_whisper import WhisperModel
-                self.audio_model = WhisperModel("large-v3", device="cuda", compute_type="float16")            
+                self.audio_model = WhisperModel('large-v3', device='cuda', compute_type='float16')            
                 self.faster = True    # Only set the flag if we succesfully imported the library and opened the model.
 
             except ImportError:
-                self.logger.error("faster_whisper not installed, falling back to whisper")
+                print('[ERROR] faster_whisper not installed, falling back to whisper')
                 self.audio_model = None
 
         if self.audio_model is None:
@@ -48,18 +44,18 @@ class WhisperMic:
             self.audio_model = whisper.load_model(model).to(device)
 
         self.audio_queue = queue.Queue()
-        self.result_queue: "queue.Queue[str]" = queue.Queue()
+        self.result_queue: 'queue.Queue[str]' = queue.Queue()
 
         self.break_threads = False
         self.mic_active = False
 
-        self.banned_results = [""," ","\n",None]
+        self.banned_results = ['',' ','\n',None]
 
         self.__setup_mic(mic_index)
 
     def __setup_mic(self, mic_index):
         if mic_index is None:
-            self.logger.info("No mic index provided, using default")
+            print('[INFO] No mic index provided, using default')
 
         self.source = sr.Microphone(sample_rate=16000, device_index=mic_index)
         self.recorder = sr.Recognizer()
@@ -70,13 +66,14 @@ class WhisperMic:
         with self.source:
             self.recorder.adjust_for_ambient_noise(self.source)
 
-        self.logger.info("Mic setup complete")
+        print('[INFO] Mic setup complete')
 
     # Whisper takes a Tensor while faster_whisper only wants an NDArray
     def __preprocess(self, data):
         is_audio_loud_enough = self.is_audio_loud_enough(data)
         if self.faster:
             return np.frombuffer(data, np.int16).flatten().astype(np.float32) / 32768.0,is_audio_loud_enough
+
         else:
             return torch.from_numpy(np.frombuffer(data, np.int16).flatten().astype(np.float32) / 32768.0),is_audio_loud_enough
 
@@ -101,7 +98,7 @@ class WhisperMic:
         return data
 
     # This method takes the recorded audio data, converts it into raw format and stores it in a queue. 
-    def __record_load(self,_, audio: sr.AudioData) -> None:
+    def __record_load(self, _, audio: sr.AudioData) -> None:
         data = audio.get_raw_data()
         self.audio_queue.put_nowait(data)
 
@@ -112,13 +109,14 @@ class WhisperMic:
 
             self.__transcribe()
 
-    def __transcribe(self,data=None, realtime: bool = False) -> None:
+    def __transcribe(self, data=None) -> None:
         if data is None:
             audio_data = self.__get_all_audio()
+
         else:
             audio_data = data
 
-        audio_data,is_audio_loud_enough = self.__preprocess(audio_data)
+        audio_data, is_audio_loud_enough = self.__preprocess(audio_data)
 
         if is_audio_loud_enough:
             predicted_text = ''
@@ -130,12 +128,12 @@ class WhisperMic:
 
             else:
                 if self.language is not None:
-                    result = self.audio_model.transcribe(audio_data, language=self.language, suppress_tokens="")
+                    result = self.audio_model.transcribe(audio_data, language=self.language, suppress_tokens='')
 
                 else:
-                    result = self.audio_model.transcribe(audio_data,suppress_tokens="")
+                    result = self.audio_model.transcribe(audio_data,suppress_tokens='')
 
-                predicted_text = result["text"]
+                predicted_text = result['text']
 
             if predicted_text not in self.banned_results:
                 self.result_queue.put_nowait(predicted_text)
@@ -146,7 +144,7 @@ class WhisperMic:
 
     def listen_continuously(self, phrase_time_limit=None):
         self.recorder.listen_in_background(self.source, self.__record_load, phrase_time_limit=phrase_time_limit)
-        self.logger.info("Listening...")
+        print('[INFO] Listening...')
         threading.Thread(target=self.__transcribe_forever, daemon=True).start()
 
         while True:
